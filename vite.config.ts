192c6160e0da329ba14d7146f2bcd83f74829812
Sébastien Chopin <atinux@gmail.com>
2025-10-08 12:48:28 +0200

    Revert "chore: revert to not use nitro options"
    This reverts commit e561c46da16f1bdc88b3e54248797d5c3f9ff62d.

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,11 @@ export default defineConfig({
         }
       }
     }),
-    nitro(),
+    nitro({
+      config: {
+        routesDir: './server/routes',
+      }
+    }),
     vueDevtools()
   ]
 })
