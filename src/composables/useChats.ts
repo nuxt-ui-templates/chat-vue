a6e545fb618a6c7f408030dd257b90ccb92578bd
Benjamin Canac <canacb1@gmail.com>
2025-11-24 10:42:23 +0100

    up

--- src/composables/useChats.ts
+++ src/composables/useChats.ts
@@ -14,7 +14,7 @@ export const useChats = createSharedComposable(() => {
   const chats = ref<Chat[]>([])
 
   const fetchChats = async () => {
-    chats.value = await $fetch('/api/chats').then(data => data.map((chat: any) => ({
+    chats.value = await $fetch('/api/chats').then(data => data.map((chat: Chat) => ({
       id: chat.id,
       label: chat.title || 'Untitled',
       to: `/chat/${chat.id}`,
