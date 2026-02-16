41368277e68ee95cd17d3ab99756cd5bd2380a90
Benjamin Canac <canacb1@gmail.com>
2026-02-16 18:21:22 +0100

    feat(app): update models list

--- src/composables/useModels.ts
+++ src/composables/useModels.ts
@@ -19,7 +19,7 @@ export function useModels() {
   const models = [
     'openai/gpt-5-nano',
     'anthropic/claude-haiku-4.5',
-    'google/gemini-2.5-flash'
+    'google/gemini-3-flash'
   ]
 
   const model = useStorage<string>('model', 'openai/gpt-5-nano')
